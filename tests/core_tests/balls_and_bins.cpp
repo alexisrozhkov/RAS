e1ece9cb5bc54cbc4375ed685683f0f7646a8f93
Alexey Rozhkov <alexisrozhkov@gmail.com>
2016-02-22 13:27:24 +0300

    use parametrized tests for balls_and_bins

--- tests/core_tests/balls_and_bins.cpp
+++ tests/core_tests/balls_and_bins.cpp
@@ -6,93 +6,116 @@
 #include <balls_and_bins.h>
 
 
-bool isIntMatrixEqual(const cv::Mat1i& a, const cv::Mat1i& b) {
-  if(a.type() != b.type()) return 0;
-  if((a.cols != b.cols) || (a.rows != b.rows)) return 0;
+const std::vector<int> expectedVals[3][3] = {
+    {
+        {1},
+
+        {1, 0,
+         0, 1},
+
+        {1, 0, 0,
+         0, 1, 0,
+         0, 0, 1}
+    },
+
+    {
+        {2},
+
+        {2, 0,
+         1, 1,
+         0, 2},
+
+        {2, 0, 0,
+         1, 1, 0,
+         1, 0, 1,
+         0, 2, 0,
+         0, 1, 1,
+         0, 0, 2}
+    },
+
+    {
+        {3},
+
+        {3, 0,
+         2, 1,
+         1, 2,
+         0, 3},
+
+        {3, 0, 0,
+         2, 1, 0,
+         2, 0, 1,
+         1, 2, 0,
+         1, 1, 1,
+         1, 0, 2,
+         0, 3, 0,
+         0, 2, 1,
+         0, 1, 2,
+         0, 0, 3}
+    }
+};
+
+cv::Mat1i getExpectedMat(const int balls, const int bins) {
+  assert(balls >= 1 && balls <= 3);
+  assert(bins >= 1 && bins <= 3);
+
+  auto expectedVec = expectedVals[balls-1][bins-1];
+  int rows = int(expectedVec.size())/bins;
+
+  cv::Mat1i expectMat = cv::Mat1i(expectedVec).t();
+  return expectMat.reshape(0, rows);
+}
+
+testing::AssertionResult isIntMatrixEqual(const cv::Mat1i& a, const cv::Mat1i& b) {
+  if(a.type() != b.type()) {
+    return testing::AssertionFailure() << testing::Message("Matrix type mismatch");
+  }
+
+  if((a.cols != b.cols) || (a.rows != b.rows)) {
+    return testing::AssertionFailure() << testing::Message("Matrix size mismatch");
+  }
 
   for(int j = 0; j < a.rows; j++) {
     for(int i = 0; i < a.cols; i++) {
-      if(a(j, i) != b(j, i)) return 0;
+      if(a(j, i) != b(j, i)) {
+        return testing::AssertionFailure() << testing::Message("Matrix content mismatch");
+      }
     }
   }
 
-  return 1;
+  return testing::AssertionSuccess();
 }
 
-TEST(resCheck, check_11) {
-  int expectedVals[] = {1};
+class BallsAndBinsTest : public testing::TestWithParam<cv::Size>
+{
+ public:
+  virtual void SetUp(){}
+  virtual void TearDown(){}
+};
 
-  auto res = balls_and_bins(1, 1);
-
-  EXPECT_EQ(res.size(), 1);
-  EXPECT_EQ(isIntMatrixEqual(res[0], cv::Mat1i(1, 1, expectedVals)), 1);
+void checkOutput(const cv::Mat1i& expected, const std::vector<cv::Mat1i>& result, const int expectNum) {
+  EXPECT_EQ(result.size(), expectNum);
+  EXPECT_TRUE(isIntMatrixEqual(result.back(), expected));
 }
 
-TEST(resCheck, check_12) {
-  int expectedVals[] = {1, 0,
-                        0, 1};
+INSTANTIATE_TEST_CASE_P(First33Arguments,
+                        BallsAndBinsTest,
+                        ::testing::Values(cv::Size(1, 1),
+                                          cv::Size(1, 2),
+                                          cv::Size(1, 3),
+                                          cv::Size(2, 1),
+                                          cv::Size(2, 2),
+                                          cv::Size(2, 3),
+                                          cv::Size(3, 1),
+                                          cv::Size(3, 2),
+                                          cv::Size(3, 3)));
 
-  auto res = balls_and_bins(1, 2);
+TEST_P(BallsAndBinsTest, checkResult)
+{
+  unsigned int balls = uint(GetParam().width),
+               bins = uint(GetParam().height);
 
-  EXPECT_EQ(res.size(), 1);
-  EXPECT_EQ(isIntMatrixEqual(res[0], cv::Mat1i(2, 2, expectedVals)), 1);
-}
+  cv::Mat1i expectMat = getExpectedMat(balls, bins);
 
-TEST(resCheck, check_21) {
-  int expectedVals[] = {2};
-
-  auto res = balls_and_bins(2, 1);
-
-  EXPECT_EQ(res.size(), 1);
-  EXPECT_EQ(isIntMatrixEqual(res[0], cv::Mat1i(1, 1, expectedVals)), 1);
-}
-
-TEST(resCheck, check_22) {
-  int expectedVals[] = {2, 0,
-                        1, 1,
-                        0, 2};
-
-  auto res = balls_and_bins(2, 2);
-
-  EXPECT_EQ(res.size(), 1);
-  EXPECT_EQ(isIntMatrixEqual(res[0], cv::Mat1i(3, 2, expectedVals)), 1);
-}
-
-TEST(resCheck, check_31) {
-  int expectedVals[] = {3};
-
-  auto res = balls_and_bins(3, 1);
-
-  EXPECT_EQ(res.size(), 1);
-  EXPECT_EQ(isIntMatrixEqual(res[0], cv::Mat1i(1, 1, expectedVals)), 1);
-}
-
-TEST(resCheck, check_32) {
-  int expectedVals[] = {3, 0,
-                        2, 1,
-                        1, 2,
-                        0, 3};
-
-  auto res = balls_and_bins(3, 2);
-
-  EXPECT_EQ(res.size(), 1);
-  EXPECT_EQ(isIntMatrixEqual(res[0], cv::Mat1i(4, 2, expectedVals)), 1);
-}
-
-TEST(resCheck, check_33) {
-  int expectedVals[] = {3, 0, 0,
-                        2, 1, 0,
-                        2, 0, 1,
-                        1, 2, 0,
-                        1, 1, 1,
-                        1, 0, 2,
-                        0, 3, 0,
-                        0, 2, 1,
-                        0, 1, 2,
-                        0, 0, 3};
-
-  auto res = balls_and_bins(3, 3);
-
-  EXPECT_EQ(res.size(), 1);
-  EXPECT_EQ(isIntMatrixEqual(res[0], cv::Mat1i(10, 3, expectedVals)), 1);
+  checkOutput(expectMat, balls_and_bins(balls, bins), 1);
+  checkOutput(expectMat, balls_and_bins(balls, bins, true), balls);
 }
