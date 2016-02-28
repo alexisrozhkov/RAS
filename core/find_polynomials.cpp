ca8bdaa4d9fcb2f5bd16612256cc512f82d75160
Alexey Rozhkov <alexisrozhkov@gmail.com>
2016-02-28 13:55:49 +0300

    convert least squares method of finding polynomial
    but, since singular vectors, that are associated with smallest singular values tend to be unstable - it is not clear how to test it (they don't match between matlab and opencv svd output)

--- core/find_polynomials.cpp
+++ core/find_polynomials.cpp
@@ -2,12 +2,29 @@
 // Created by alexey on 27.02.16.
 //
 
+#include <iostream>
 #include "find_polynomials.h"
+
 
 Mat2D find_polynomials(const Mat2D &data,
                        const Mat3D &derivative,
                        const FindPolyMethod method,
                        const int charDimension) {
+  if(method == FISHER) {
+    return Mat2D();
+  }
 
-  return Mat2D();
+  else {
+    Mat2D w, u, vt;
+    cv::SVD::compute(data, w, u, vt, cv::SVD::FULL_UV);
+
+    //std::cout << w << std::endl;
+
+    Mat2D out(u.rows, charDimension);
+    for(int j = 0; j < charDimension; j++) {
+      u.col(u.cols-1-j).copyTo(out.col(charDimension-1-j));
+    }
+
+    return out;
+  }
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <perspective_embedding.h>
+#include <find_polynomials.h>
 
 using namespace std;
 
 // Matlab-friendly notation for testing
-// matchesData = [1; 2; 3; 4; 5]
-// matchesData = [1, 6; 2, 7; 3, 8; 4, 9; 5, 10]
-// matchesData = [0; 2; 3; 4; 5]
-// matchesData = [0, 6; 2, 0; 3, 8; 4, 9; 5, 10]
+// matchesData = [100, 200, 300, 400, 500, 600, 700, 800, 900; 150, 250, 350, 450, 550, 650, 750, 850, 950; 200, 300, 400, 500, 600, 700, 800, 900,1000; 350, 450, 550, 650, 750, 850, 950,1050,1150; 1,   1,   1,   1,   1,   1,   1,   1,   1]
 
 int main() {
-  // double matchesData51[] = {1, 2, 3, 4, 5};
-  double matchesData52[] = {1, 6, 2, 7, 3, 8, 4, 9, 5, 10};
-  // double matchesData051[] = {0, 2, 3, 4, 5};
-  // double matchesData052[] = {0, 6, 2, 0, 3, 8, 4, 9, 5, 10};
+  double matchesData[] = {100, 200, 300, 400, 500, 600, 700, 800, 900,
+                          150, 250, 350, 450, 550, 650, 750, 850, 950,
+                          200, 300, 400, 500, 600, 700, 800, 900,1000,
+                          350, 450, 550, 650, 750, 850, 950,1050,1150,
+                            1,   1,   1,   1,   1,   1,   1,   1,   1};
 
-  std::cout << perspective_embedding(Mat2D(5, 2, matchesData52), 1) << std::endl;
+  auto e = perspective_embedding(Mat2D(5, 9, matchesData), 1);
+  auto pol = find_polynomials(e.getV().back(), e.getD().back(), LLE, 9);
+
+  std::cout << pol << std::endl;
 
   return 0;
 }
